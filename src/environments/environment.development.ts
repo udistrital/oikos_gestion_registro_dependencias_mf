4682dc627cf1b22eee94349d5cc3331f8b3da804
Diego Gutierrez <diagutierrezro@unal.edu.co>
2024-09-09 16:13:30 -0500

    fix: Cambio de variables de entorno

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production:false,
-    apiUrl: "http://localhost:4202/",
+    apiUrl: "",
     OIKOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v2/',
     OIKOS_MID: 'http://localhost:8080/v1/'
 };
